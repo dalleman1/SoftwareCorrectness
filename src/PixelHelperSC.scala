4f8409a5aabd1211c7cae91a8650aaa61644ff60
Morten Dalsgaard <monglade.dalsgaard@gmail.com>
2023-04-20 11:14:26 +0200

    Added the Draw method - not finished yet and wont compile since Scala doesnt work on my PC as it is. So small changes are needed for it to compile.

--- src/PixelHelperSC.scala
+++ src/PixelHelperSC.scala
@@ -4,7 +4,8 @@ import java.awt._
 class PixelHelperSC {
   private var pan: JPanel = null
   private var boundaryBox: BoundaryBox = null //new BoundaryBox(0,0,0,0)
-
+  private var shapes: List[CustomShape] = List()
+  private var objectCounter = 0
   def this(panel: JPanel) {
     this()
     pan = panel
@@ -15,16 +16,33 @@ class PixelHelperSC {
     DrawRectangle(x1, y1, x2, y2)
   }
 
-  def doPixel(x: Int, y: Int): Unit = {
+  def doPixel(x: Int, y: Int, color: Color = Color.BLACK): Unit = {
     if(boundaryBox.insideBox(x, y)) {
       val g2d = pan.getGraphics.create.asInstanceOf[Graphics2D]
       g2d.translate(0, pan.getHeight)
       g2d.scale(1, -1)
+      g2d.setColor(color)
       g2d.drawRect(x, y, 1, 1)
       g2d.dispose()
     }
   }
 
+
+  def Draw(input : String, color : String): Unit = {
+    // Parse the color string to a Color object
+    val parsedColor = Color.decode(color)
+    val shapeIds = input.split(",")
+
+    for (shapeId <- shapeIds) {
+      val shapeOpt = shapes.find(_.id == shapeId)
+      shapeOpt.foreach(shape => shape match {
+        case rect: Rectangle => DrawRectangle()
+        case circle: Circle => DrawCircle()
+        case line: Line => DrawLine()
+      })
+    }
+  }
+// color: Color = Color.BLACK should be included in drawline, circle and rectangle.
   def DrawLine(x1: Int, y1: Int, x2: Int, y2: Int): Unit = {
     var x1_mod = x1
     var y1_mod = y1
@@ -52,6 +70,8 @@ class PixelHelperSC {
         y1_mod += sy
       }
     }
+    val line = Line("o" + objectCounter, x1, y1, x2, y2)
+    shapes = line :: shapes
   }
 
   def DrawCircle(inputX: Int, inputY: Int, r: Int): Unit = {
@@ -74,6 +94,8 @@ class PixelHelperSC {
         decisionOver2 += 2 * (y - r_mod) + 1
       }
     }
+    val circle = Circle("o" + objectCounter, inputX, inputY, r)
+    shapes = circle :: shapes
   }
 
   def DrawRectangle(x1: Int, y1: Int, x2: Int, y2: Int): Unit = {
@@ -85,6 +107,9 @@ class PixelHelperSC {
     DrawLine(x2, y2, x2, y1)
     //Bottom right
     DrawLine(x1, y1, x2, y1)
+
+    val line = Rectangle("o" + objectCounter, x1, y1, x2, y2)
+    shapes = line :: shapes
   }
 
 }
